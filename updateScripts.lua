4b85fb12be5e9d65316a89ceca11f4d49614fb1c
Phaysik <mmoore0111@gmail.com>
2023-01-28 15:59:05 -0600

    Reduce updateScripts by a bit

--- updateScripts.lua
+++ updateScripts.lua
@@ -13,82 +13,69 @@ local headers = {
     ["cache-control"] = "no-store"
 }
 
-http.request { url = "https://github.com/" .. githubName .. "/" .. repositoryName .. "/", headers = headers } -- Where all lua scripts are hosted
-
-local requesting = true
+local repoRequest = http.get("https://github.com/" .. githubName .. "/" .. repositoryName .. "/", headers) -- Where all lua scripts are hosted
 
 local debug = false
 
 local file = fs.open("Debug/siteInfo.json", "w") -- Debug file
 
-while requesting do
-    ---@diagnostic disable-next-line: undefined-field
-    local statusCode, url, sourceText = os.pullEvent()
+if repoRequest then
+    local line = repoRequest.readLine()
 
-    if statusCode == "http_success" then
-        local line = sourceText.readLine()
+    local files = {}
 
-        local files = {}
-
-        while line do -- Loop through all lines returned by the request
-            if string.find(line, ".lua") then
-                local regex = "a\">.*.lua" -- Get a\">[fileName].lua from any line that has a .lua
-                local fileName = ""
-                for token in string.gmatch(line, regex) do
-                    fileName = string.sub(token, 4) -- Remove the a\"> from the string
-
-                    if debug then
-                        print(fileName)
-                    end
-                end
-
-                table.insert(files, fileName) -- Compile list of all files
-                line = sourceText.readLine()
-            else
-                line = sourceText.readLine()
-            end
-        end
-
-        sourceText.close()
-
-        if debug then
-            file.write(textutils.serialize(files))
-        end
-
-        for _, value in pairs(files) do -- Loop through all lua files
-            if value ~= "" then -- Ignore the empty filename that is somehow added
-                local concatenation = "https://raw.githubusercontent.com/" ..
-                    githubName .. "/" .. repositoryName .. "/" .. devBranch .. "/" .. value -- Get the raw data of the lua files
+    while line do -- Loop through all lines returned by the request
+        if string.find(line, ".lua") then
+            local regex = "a\">.*.lua" -- Get a\">[fileName].lua from any line that has a .lua
+            local fileName = ""
+            for token in string.gmatch(line, regex) do
+                fileName = string.sub(token, 4) -- Remove the a\"> from the string
 
                 if debug then
-                    file.write(value)
-                    print(concatenation)
-                end
-
-                local request = http.get(concatenation, headers)
-
-                if request then
-                    local content = request.readAll() -- Read all lines in the lua file
-
-                    local scriptFile = fs.open(value, "w")
-
-                    scriptFile.write(content)
-
-                    scriptFile.close()
-
-                    print("File: " .. value .. " downloaded successfully")
-                else
-                    print("File: " .. value .. " not found")
+                    print(fileName)
                 end
             end
+
+            table.insert(files, fileName) -- Compile list of all files
+            line = repoRequest.readLine()
+        else
+            line = repoRequest.readLine()
         end
-
-        file.close()
-
-        requesting = false
-    elseif statusCode == "http_failure" then
-        print("Request failed")
-
-        requesting = false
     end
+
+    repoRequest.close()
+
+    if debug then
+        file.write(textutils.serialize(files))
+    end
+
+    for _, value in pairs(files) do -- Loop through all lua files
+        if value ~= "" then -- Ignore the empty filename that is somehow added
+            local concatenation = "https://raw.githubusercontent.com/" ..
+                githubName .. "/" .. repositoryName .. "/" .. devBranch .. "/" .. value -- Get the raw data of the lua files
+
+            if debug then
+                file.write(value)
+            end
+
+            local request = http.get(concatenation, headers)
+
+            if request then
+                local content = request.readAll() -- Read all lines in the lua file
+
+                local scriptFile = fs.open(value, "w")
+
+                scriptFile.write(content)
+
+                scriptFile.close()
+
+                print("File: " .. value .. " downloaded successfully")
+            else
+                print("File: " .. value .. " not found")
+            end
+        end
+    end
+
+    file.close()
+
 end
